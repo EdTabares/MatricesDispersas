b331e313d68bfe8fc57d478b1aa023611057d040
Ed Tabares <edward@MacBook-Air-de-Edward.local>
2022-06-09 14:29:45 -0500

    Tabares <edward@MacBook-Air-de-Edward.local>

---
+++ src/matricesdispersas/MatricesDispersas.java
@@ -0,0 +1,135 @@
+package matricesdispersas;
+
+import java.io.*;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
+public class MatricesDispersas {
+
+    public static void main(String[] args) {
+
+        menuppal();
+    }
+
+    public static void menuppal() {
+        int opcion = 0;
+        String menu = "***MENU PRINCIPAL MATRICES DISPERSAS***\n"
+                + "1- MD Tripleta\n"
+                + "2- MD Forma 1\n"
+                + "3- MD Forma 2\n"
+                + "0- Salir";
+        do {
+            try {
+                opcion = Integer.parseInt(JOptionPane.showInputDialog(menu));
+                switch (opcion) {
+                    case 1:
+                        menuTripleta();
+                        break;
+                    case 2:
+                        menuForma1();
+                        break;
+                    case 3:
+                        menuForma2();
+                        break;
+                    case 0:
+                        System.exit(0);
+                        break;
+                    default:
+                        JOptionPane.showMessageDialog(null, "Opción no válida");
+                }
+
+            } catch (NumberFormatException ex) {
+                JOptionPane.showMessageDialog(null, "Opción no válida");
+            }
+        } while (opcion != 0);
+    }
+
+    public static void menuTripleta() {
+        int[][]mat = leerArchivo();
+        int row, data, col;
+        Tripleta T = new Tripleta(mat);
+        int opcion = -1;
+        String menu = "***MENU TRIPLETA***\n"
+                + "1- Mostrar Tripleta\n"
+                + "2- Suma de Filas\n"
+                + "3- Suma de Columnas\n"
+                + "4- Insertar Dato\n"
+                + "5- Eliminar Dato\n"
+                + "6- Sumar 2 tripletas\n"
+                + "7- Volver al menú anterior\n"
+                + "0- Salir";
+
+        do {
+            try {
+                opcion = Integer.parseInt(JOptionPane.showInputDialog(menu));
+                switch (opcion) {
+                    case 1: T.mostrarTripleta();
+                        break;
+                    case 2: T.sumarFilas();
+                        break;
+                    case 3: T.sumarColumnas();
+                        break;
+                    case 4: row = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero de fila (Considerela desde 0)"));
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    case 7: menuppal();
+                        break;
+                    case 0: System.exit(0);
+                        break;
+                    default:
+                        JOptionPane.showMessageDialog(null, "opcion no válida");
+                }
+            }catch(NumberFormatException ex){
+                JOptionPane.showMessageDialog(null,"opcion no válida"); 
+            }
+    }
+    while(opcion!=0);
+    }
+    
+    public static void menuForma1(){
+        
+    }
+    
+    public static void menuForma2(){
+        
+    }
+
+    public static int[][] leerArchivo() {
+        int[][] matrix = null;
+        try {
+            FileReader r = new FileReader("Matriz.txt");
+            BufferedReader buffer = new BufferedReader(r);
+
+            String temp = "";
+            ArrayList<String> lines = new ArrayList<>();
+
+            while ((temp = buffer.readLine()) != null) {
+                lines.add(temp);
+            }
+            String[] numbers = lines.get(0).split(",");
+            int x = lines.size(); // Cantidad de lineas (Filas)
+            int y = numbers.length; //Cantidad de números por linea (Columnas)            
+            matrix = new int[x][y];
+
+            //Llenamos la matriz linea por linea 
+            for (int i = 0; i < x; i++) {
+                String[] line = lines.get(i).split(",");
+                for (int j = 0; j < y; j++) {
+                    matrix[i][j] = Integer.parseInt(line[j]);
+                    System.out.print(matrix[i][j] + " | ");
+                }
+                System.out.println("\n");
+            }     
+          //  Tripleta t = new Tripleta(matrix);
+        } catch (FileNotFoundException e) {
+            JOptionPane.showMessageDialog(null, "No se ha encontrado el archivo");
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "Warning");
+        }             
+        return matrix;
+    }
+
+}
---
+++ src/matricesdispersas/Tripleta.java
@@ -0,0 +1,161 @@
+package matricesdispersas;
+
+import javax.swing.JOptionPane;
+
+public class Tripleta {
+
+    private int filas;
+    private int columnas;
+    private int cantDatos = 0;
+    private int mat[][];
+    private int trip[][];
+
+    public Tripleta() {
+
+    }
+
+    public Tripleta(int n, int m, int cantDatos, int mat[][]) {
+        filas = n;
+        columnas = m;
+        this.cantDatos = cantDatos;
+        trip = new int[cantDatos][3];
+        trip[0][0] = filas;
+        trip[0][1] = columnas;
+        trip[0][2] = this.cantDatos; // Validar si lo inicialiamos en cero según el Blog ¿?
+    }
+
+    public Tripleta(int[][] mat) {
+        this.mat = mat;
+        cantDatos = calcularCantDatos();
+        trip = new int[cantDatos + 1][3];
+        columnas = this.mat.length; // Cambie entre columnas y filas 
+        filas = this.mat[0].length;
+        generarTripleta();
+    }
+
+    public int getFilas() {
+        return filas;
+    }
+
+    public void setFilas(int filas) {
+        this.filas = filas;
+    }
+
+    public int getColumnas() {
+        return columnas;
+    }
+
+    public void setColumnas(int columnas) {
+        this.columnas = columnas;
+    }
+
+    public int getCantDatos() {
+        return cantDatos;
+    }
+
+    public void setCantDatos(int cantDatos) {
+        this.cantDatos = cantDatos;
+    }
+
+    public int[][] getMat() {
+        return mat;
+    }
+
+    public void setMat(int[][] mat) {
+        this.mat = mat;
+    }
+
+    public int[][] getTrip() {
+        return trip;
+    }
+
+    public void setTrip(int[][] trip) {
+        this.trip = trip;
+    }
+
+    private int calcularCantDatos() {
+        for (int i = 0; i < mat[0].length; i++) {
+            for (int j = 0; j < mat.length; j++) {
+                if (mat[i][j] != 0) {
+                    cantDatos++;
+                }
+            }
+        }
+        return cantDatos;
+    }
+
+    private void generarTripleta() {
+        trip[0][0] = filas;
+        trip[0][1] = columnas;
+        trip[0][2] = cantDatos;
+
+        int k = 1;
+
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                if (mat[i][j] != 0 && k <= cantDatos) {
+                    trip[k][0] = i;
+                    trip[k][1] = j;
+                    trip[k][2] = mat[i][j];
+                    k++;
+                }
+            }
+        }
+    }
+
+    public void mostrarTripleta() {
+        //  for (int i = 0; i <= cantDatos; i++) {
+        // System.out.print(trip[i][0] + "\t" + trip[i][1] + "\t" + trip[i][2]);
+        //System.out.println("");
+        //       }
+
+        String Tripleta = "";
+        for (int i = 0; i < (cantDatos + 1); i++) {
+            for (int j = 0; j < 3; j++) {
+                Tripleta += trip[i][j];
+                Tripleta += "         ";
+            }
+            Tripleta += "\n";
+        }
+        JOptionPane.showMessageDialog(null, "*** TRIPLETA ***\n\n" + Tripleta);
+    }
+
+    public void sumarFilas() {
+        String aux = "", message = "";
+        int k = 0;
+        while (k < filas) {
+            int sumaFila = 0;
+            for (int i = 0; i < (cantDatos + 1); i++) {
+                if (trip[i][0] == k) {
+                    sumaFila = sumaFila + trip[i][2];
+                }
+            }
+            message = "La suma de la Fila " + k + " es: " + sumaFila;
+            aux += String.valueOf(message) + "\n";
+            k++;
+        }
+        JOptionPane.showMessageDialog(null, "**** SUMAS FILAS ****\n\n" + aux);
+    }
+
+    public void sumarColumnas() {
+        String aux = "", message = "";
+        int k = 0;
+        while (k < columnas) {
+            int sumaCol = 0;
+            for (int i = 0; i < (cantDatos + 1); i++) {
+                if (trip[i][1] == k) {
+                    sumaCol = sumaCol + trip[i][2];
+                }
+            }
+            message = "La suma de la Columna " + k + " es: " + sumaCol;
+            aux += String.valueOf(message) + "\n";
+            k++;
+        }
+        JOptionPane.showMessageDialog(null, "**** SUMAS COLUMNAS ****\n\n" + aux);
+    }
+    
+    public void insertarDato(int row, int col, int data){
+        
+    }
+
+}
