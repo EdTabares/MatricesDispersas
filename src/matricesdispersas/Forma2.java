05d0a5840e5661341d6a9596a547413921affbaa
Ed Tabares <edward@MacBook-Air-de-Edward.local>
2022-06-20 14:37:51 -0500

    Forma2 Update

--- src/matricesdispersas/Forma2.java
+++ src/matricesdispersas/Forma2.java
@@ -1,21 +1,19 @@
-
 package matricesdispersas;
 
-
 public class Forma2 {
-    
+
     Nodo punta;
-    
-    public void Forma2(){
+
+    public void Forma2() {
         punta = null;
     }
-    
-    public Nodo getPunta(){
+
+    public Nodo getPunta() {
         return punta;
     }
-    
-    public void generarForma1(int mat[], int row, int col){
-        int i=0, j, cont=0;
+
+    public void generarForma2(int mat[][], int row, int col) {
+        int i = 0, j, cont = 0;
         Nodo primer = new Nodo();
         Nodo p = new Nodo();
         Nodo ant = new Nodo();
@@ -23,16 +21,27 @@ public class Forma2 {
         primer.setColumna(col);
         punta = primer;
         p = punta;
-        
-        while(j < mat.length){
-            if (mat[i][j] != 0) {
-                Nodo x = new Nodo();
-                x.setFila(i);
-                x.setColumna(j);
-                
+
+        while (i < row) {
+            j = 0;
+            while (j < col) {
+                if (mat[i][j] != 0) {
+                    Nodo x = new Nodo();
+                    x.setFila(i);
+                    x.setColumna(j);
+                    x.setDato(mat[i][j]);
+                    p.setLf(x);
+                    p = x;
+                }
+                j++;
             }
+            i++;
+        }
+        p.setLf(punta);
+        ant = punta;
+        while(cont < punta.getColumna()){
+            
         }
     }
-    
-    
+
 }
